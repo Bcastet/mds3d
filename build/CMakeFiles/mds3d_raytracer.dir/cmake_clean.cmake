c247b5c6cf684041454db8c3989049ca8da8e91d
bcastet <benjamin.castet@etu.u-bordeaux.fr>
2020-01-24 01:22:54 +0100

    Maybe not so stupid

---
+++ build/CMakeFiles/mds3d_raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mds3d_raytracer.dir/src/common.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/block.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/viewer.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/bitmap.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/camera.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/main.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/object.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/shape.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/sphere.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/plane.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/scene.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/material.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/directionalLight.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/pointLight.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/phong.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/parser.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/proplist.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/flat.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/normal.cpp.o"
+  "CMakeFiles/mds3d_raytracer.dir/src/direct.cpp.o"
+  "mds3d_raytracer.pdb"
+  "mds3d_raytracer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mds3d_raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/DependInfo.cmake
@@ -0,0 +1,89 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/nanovg/src/nanovg.c" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/ext/nanovg/src/nanovg.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CORO_SJLJ"
+  "NANOGUI_BUILD"
+  "NVG_BUILD"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/eigen"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/glfw/include"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/nanovg/src"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/include"
+  "ext_build/nanogui"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/coro"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/nanogui_resources.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/nanogui_resources.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/button.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/button.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/checkbox.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/checkbox.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/colorpicker.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/colorpicker.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/colorwheel.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/colorwheel.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/combobox.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/combobox.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/common.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/common.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/glcanvas.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/glcanvas.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/glutil.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/glutil.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/graph.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/graph.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/imagepanel.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/imagepanel.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/imageview.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/imageview.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/label.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/label.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/layout.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/layout.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/messagedialog.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/messagedialog.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/popup.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/popup.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/popupbutton.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/popupbutton.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/progressbar.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/progressbar.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/screen.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/screen.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/serializer.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/serializer.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/slider.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/slider.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/stackedwidget.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/stackedwidget.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/tabheader.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/tabheader.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/tabwidget.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/tabwidget.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/textbox.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/textbox.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/theme.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/theme.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/vscrollpanel.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/vscrollpanel.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/widget.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/widget.cpp.o"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/src/window.cpp" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/CMakeFiles/nanogui-obj.dir/src/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CORO_SJLJ"
+  "NANOGUI_BUILD"
+  "NVG_BUILD"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/eigen"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/glfw/include"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/nanovg/src"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/include"
+  "ext_build/nanogui"
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/mds3d_td1/ext/nanogui/ext/coro"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/nanogui_resources.h" "/autofs/unityaccount/cremi/bcastet/Bureau/MDS3D/build/ext_build/nanogui/nanogui_resources.cpp"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
